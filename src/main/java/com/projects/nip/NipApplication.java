ce9c3b1f63cb10bb25d423770ad3821f5235e8cc
oluexpert99 <makavelidason@gmail.com>
2018-03-21 12:59:12 +0100

    first commit

---
+++ src/main/java/com/projects/nip/NipApplication.java
@@ -0,0 +1,12 @@
+package com.projects.nip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NipApplication.class, args);
+	}
+}
